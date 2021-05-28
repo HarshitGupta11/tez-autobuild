4def9161e0568a63d560afba4d551864291cdbd2
HarshitGupta11 <gupta.harshit231199@gmail.com>
2021-05-28 23:06:26 +0530

    Added basic hadoop setup scripts

--- hadoop_setup.sh
+++ hadoop_setup.sh
@@ -1,8 +1,22 @@
-test -d hadoop-3.1.4 || wget https://downloads.apache.org/hadoop/common/hadoop-3.1.4/hadoop-3.1.4.tar.gz 
+#new update
+# download the binaries
+ssh-keygen -f ~/key_file -P ""
+cat ~/key_file >> $HOME/.ssh/authorized_keys
+ssh localhost
+test -d hadoop-3.1.4 || wget https://downloads.apache.org/hadoop/common/hadoop-3.1.4/hadoop-3.1.4.tar.gz
 tar -xf hadoop-3.1.4.tar.gz
 cd hadoop-3.1.4/bin
-export PATH=$PATH:`pwd`
+export PATH=`pwd`:$PATH
 cd ../etc/hadoop
-printf "export JAVA_HOME=`dirname $(dirname $(readlink -f $(which javac)))`" >> hadoop_env.sh
-export JAVA_HOME=`dirname $(dirname $(readlink -f $(which javac)))`"/jre"
+echo "export JAVA_HOME=`dirname $(dirname $(readlink -f $(which javac)))`" >> hadoop-env.sh
+mv /tmp/core-site.xml /tmp/mapred-site.xml /tmp/hdfs-site.xml .
 cd ../../..
+export JAVA_HOME=`dirname $(dirname $(readlink -f $(which javac)))`
+hadoop namenode -format
+export HDFS_NAMENODE_USER="hduser"
+export HDFS_DATANODE_USER="hduser"
+export HDFS_SECONDARYNAMENODE_USER="hduser"
+export YARN_RESOURCEMANAGER_USER="hduser"
+export YARN_NODEMANAGER_USER="hduser"
+./hadoop/sbin/start-all.sh
+#1 2 2 4 5 17
---
+++ hadoop_start.sh
@@ -0,0 +1,11 @@
+#this will add the user and copy the  setup script in the tmp directory where it will
+#be picked up hduser to execute
+addgroup hadoop
+adduser --ingroup hadoop hduser
+mkdir -p /app/hadoop/tmp
+chown 750 /app/hadoop/tmp
+test -d /tmp || mkdir -p /tmp
+cp hadoop_setup.sh conf_files/hdfs-site.xml conf_files/mapred-site.xml conf_files/hdfs-site.xml /tmp
+chmod 777 /tmp/hadoop_setup.sh
+su -c hduser "./tmp/hadoop_setup.sh"
+rm /tmp/hadoop_setup.sh
