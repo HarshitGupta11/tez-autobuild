bd26894b8ada0c4130019aff70694ad4d11b87a5
HarshitGupta11 <gupta.harshit231199@gmail.com>
2021-05-29 19:19:34 +0530

    Fixes

--- hadoop_start.sh
+++ hadoop_start.sh
@@ -7,11 +7,12 @@ mkdir /home/hduser
 useradd -g hadoop -s /bin/bash -d /home/hduser hduser
 chown hduser /app/hadoop/tmp
 chmod 750 /app/hadoop/tmp
+chown hduser /home/hduser
 test -d /tmp || mkdir -p /tmp
 cp hadoop_setup.sh conf_files/core-site.xml conf_files/mapred-site.xml conf_files/hdfs-site.xml /tmp
 chmod 777 /tmp/hadoop_setup.sh
 su -c "echo -ne '\n' | ssh-keygen -t rsa -P \"\" "  hduser
-su -c "cat ~\/.ssh\/id_rsa.pub >> ~\/.ssh\/authorized_keys" hduser
+su -c "cat \/home\/hduser\/.ssh\/id_rsa.pub >> \/home\/hduser\/.ssh\/authorized_keys" hduser
 service ssh restart
 su -c ". /tmp/hadoop_setup.sh" hduser
 rm /tmp/hadoop_setup.sh
