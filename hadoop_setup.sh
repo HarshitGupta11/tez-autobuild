ad9f7c73b247317c88e2a6c9ec370cce51cd3990
HarshitGupta11 <gupta.harshit231199@gmail.com>
2021-05-29 17:23:57 +0530

    Fixes

--- hadoop_setup.sh
+++ hadoop_setup.sh
@@ -1,11 +1,10 @@
 #new update
 # download the binaries
-ssh-keygen -f ~/key_file -P ""
-cat ~/key_file >> $HOME/.ssh/authorized_keys
 ssh localhost
 test -d hadoop-3.1.4 || wget https://downloads.apache.org/hadoop/common/hadoop-3.1.4/hadoop-3.1.4.tar.gz
 tar -xf hadoop-3.1.4.tar.gz
-cd hadoop-3.1.4/bin
+mv hadoop-3.1.4 hadoop
+cd hadoop/bin
 export PATH=`pwd`:$PATH
 cd ../etc/hadoop
 echo "export JAVA_HOME=`dirname $(dirname $(readlink -f $(which javac)))`" >> hadoop-env.sh
--- hadoop_start.sh
+++ hadoop_start.sh
@@ -1,12 +1,12 @@
 #this will add the user and copy the  setup script in the tmp directory where it will
 #be picked up hduser to execute
-apt update
-apt install -y wget ssh < inputs_hadoop
-./install_jdk.sh
+service ssh start
 addgroup hadoop
-adduser --ingroup hadoop -p "" hduser
+mkdir /home/hadoop
+useradd -g hadoop -s /bin/bash -d /home/hduser hduser
 mkdir -p /app/hadoop/tmp
-chown 750 /app/hadoop/tmp
+chown hduser /app/hadoop/tmp
+chmod 750 /app/hadoop/tmp
 test -d /tmp || mkdir -p /tmp
 cp hadoop_setup.sh conf_files/hdfs-site.xml conf_files/mapred-site.xml conf_files/hdfs-site.xml /tmp
 chmod 777 /tmp/hadoop_setup.sh
