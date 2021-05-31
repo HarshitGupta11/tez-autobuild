8f8678b1da42018bd8b47fa5ff836e0cedb3573f
HarshitGupta11 <gupta.harshit231199@gmail.com>
2021-05-31 23:59:02 +0530

    Deletion of gen files

--- gen_files/gen_ssh.sh
+++
@@ -1,4 +0,0 @@
-echo -ne '\n' | ssh-keygen -t rsa -P "" 
-ssh-copy-id -i /home/hduser/.ssh/id_rsa.pub hduser@master
-ssh-copy-id -i /home/hduser/.ssh/id_rsa.pub hduser@slave_1
-ssh-copy-id -i /home/hduser/.ssh/id_rsa.pub hduser@slave_2
